e6aec698d0e2fedbf76b490f60d46e4291df2cc9
ubungmeister <ubungmeister@gmail.com>
2022-09-18 17:20:07 +0200

    style update

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,15 @@ function App() {
   const [text, setText] = useState('')
   const [filter, setFilter] = useState<FilterType>('All')
 
+  const getDate =()=>{
+    let month = ['January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December'][new Date().getMonth()]
+    let weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][new Date().getDay()]
+    let fullDay = weekday+', '+ new Date().getDate()+' of';
+    let date = [fullDay,month]
+
+    return date
+  }
 
   let todos = useSelector<AppRootStateType, Array<Todolist>>(state => state.todos.todo)
   const dispatch = useDispatch()
@@ -58,20 +67,21 @@ function App() {
 
   return (
       <Conteiner>
+
         <Wrapper>
-          <TitleWrapper>Todo App</TitleWrapper>
+          <BackgroundWrapper>
+            <TitleWrapper>{getDate()[0]}</TitleWrapper>
+            <TitleWrapper>{getDate()[1]}</TitleWrapper>
+          </BackgroundWrapper>
           <TodoInputWrapper
               type='text'
               onChange={event => {
                 setText(event.target.value)
               }}
+              onKeyPress={(event)=>{if(event.key === 'Enter'){addTodosHandler()}}}
               value={text}
               placeholder={'Start writing...'}
           />
-          <AddButtonWrapper onClick={addTodosHandler}
-                            whileHover={{scale: 1.1}}
-                            whileTap={{scale: 0.9}}> Add
-          </AddButtonWrapper>
           <ul> {filterTodoList(filter).map((todo) => {
             return (
                 <MapTasksWrapper key={todo.id}>
@@ -80,12 +90,13 @@ function App() {
                   {/*              callBack={(changeTitle) => {*/}
                   {/*                  editTodoListHandler(todo.id, changeTitle)*/}
                   {/*                              }}/>*/}
-
+                  <IconsWrapper>
                   <ButtonsTaskWrapper onClick={() => removeTodosHandler(todo.id)}>
                     <FaTrash size='1.2rem' color='#c13923'
                              opacity='40%' cursor='pointer'/>
                   </ButtonsTaskWrapper>
                   <CheckBoxWrapper type={"checkbox"} onClick={() => checkboxHandler(todo.id)}/>
+                  </IconsWrapper>
                 </MapTasksWrapper>
             )
           })}
@@ -117,26 +128,38 @@ export const Conteiner = styled.div`
 export const Wrapper = styled.div`
   width: 500px;
   min-height: 50vh;
-  padding: 25px;
   background-color: white;
   box-shadow: 5px 5px 30px 0px rgba(0, 0, 0, 0.5);
 
 `
+export const BackgroundWrapper = styled.div`
+  padding: 2.7em;
+  background-image: linear-gradient(90deg, rgba(224, 195, 252, 0.7) 0%, rgba(142, 197, 252, 0.7) 100%), 
+  url(https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1355&q=80);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+`
 export const TitleWrapper = styled.div`
+  font-size: 25px;
   text-align: center;
-  margin-bottom: 1rem;
-  color: darkslategray;
-  font-size: 35px;
+  margin-bottom: -0.3rem;
+  color: #ffffff;
   text-shadow: 1px 2px 5px rgba(255, 85, 105, 0.3), 3px -1px 5px rgba(80, 220, 251, 0.08);
 `
 
 export const TodoInputWrapper = styled.input`
-  width: 75%;
-  padding: 11px;
-  margin: 8px 0;
-  border: 2px solid #ccc;
-  border-radius: 4px;
-
+  //width: 70%;
+  //padding: 12px;
+  //margin: 5px 5px;
+  //border: 2px solid #ccc;
+  //border-radius: 4px;
+  background-color: rgb(237, 236, 248);
+  border: none;
+  font-size: 1.2em;
+  margin: 0;
+  padding: 1em 1em;
+  width: 100%;
 `
 export const AddButtonWrapper = styled(motion.button)`
   background-image: linear-gradient(to right, #f5f0ff, #ebf8ff);
@@ -158,19 +181,21 @@ export const MapTasksWrapper = styled.li`
   background-color: #f4f4f4;
   border-radius: 20px;
   margin: 0.5rem;
-
   &:hover {
     border: 2px solid rebeccapurple;
   }
 
 `
+
+export const IconsWrapper = styled.div`
+
+`
 export const ButtonsTaskWrapper = styled.div`
   margin-left: 365px;
   height: 20px;
-  position: fixed
+  
 `
 export const CheckBoxWrapper = styled.input`
-  position: fixed;
   height: 20px;
   margin-left: 390px;
 
